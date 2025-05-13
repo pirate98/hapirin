09290fb074b7de9edd5c73d3f83aff725ce33671
benjiro-ls <benjiro.tan.dev@gmail.com>
2025-05-13 22:04:46 +0800

    Fix Toolbar component

--- src/screens/toolbar/Toolbar.tsx
+++ src/screens/toolbar/Toolbar.tsx
@@ -1,6 +1,4 @@
 /**
- *
- * BeetSoft Co., Ltd
  * Common Toolbar
  *
  */
@@ -8,49 +6,69 @@
 import React from 'react';
 import {
   View,
-  TouchableOpacity,
   Dimensions,
   Platform,
   SafeAreaView,
   StyleSheet,
-  Image,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { moderateScale, verticalScale } from 'react-native-size-matters';
+import FastImage from '@d11/react-native-fast-image';
+
+import TextFont from '../../commons/components/TextFont';
+import TouchableDebounce from '../../commons/components/TouchableDebounce';
+import TouchableDebounceDelay from '../../commons/components/TouchableDebounce';
+import SoundService from '../../soundService/SoundService';
 import Styles from '../../styles/Styles';
 import Constants from '../../constants/Constants';
-import Navigation from '../navigation/Navigation';
-import TextFont from '../../commons/components/TextFont';
-//use scale size for screen different
-import {moderateScale, scale, verticalScale} from 'react-native-size-matters';
-import SoundService from '../../soundService/SoundService';
-import {Color} from '../../colors/Colors';
+import { Color } from '../../colors/Colors';
 import scales from '../../styles/scales';
-import FastImage from '@d11/react-native-fast-image';
-import {Actions} from 'react-native-router-flux';
-import TouchableDebounceDelay from '../../commons/components/TouchableDebounce';
-import TouchableDebounce from '../../commons/components/TouchableDebounce';
+
+interface ToolbarProps {
+  title: string;
+  leftIcon?: 'home' | 'back';
+  nameRightButton?: 'create' | 'home' | 'none';
+  onClickBackButton?: () => void;
+  onClickRightButton: () => void;
+  style?: object;
+}
 
 export const getNotchHeight = () => {
   if (Platform.OS === 'ios') {
-    const {width, height} = Dimensions.get('window');
-    return !Platform.isPad && !Platform.isTVOS && (height > 800 || width > 800)
+    const { width, height } = Dimensions.get('window');
+    return !Platform.isPad && !Platform.isTV && (height > 800 || width > 800)
       ? 30
       : 10;
   }
   return 0;
 };
 
-export default class Toolbar extends React.Component {
-  // params onClickBackButton: callback of Back button
-  renderLeftIcon(name, onClickBackButton) {
-    return (
-      <View style={{flex: 1.5}}>
-        {name === 'home' ? (
+const Toolbar: React.FC<ToolbarProps> = ({
+  title,
+  leftIcon,
+  nameRightButton = 'none',
+  onClickBackButton,
+  onClickRightButton,
+}) => {
+  const navigation = useNavigation();
+
+  const handleBackPress = async () => {
+    await SoundService.loadSoundSel('sel.mp3');
+    if (onClickBackButton) {
+      onClickBackButton();
+    } else {
+      navigation.goBack(); // fallback
+    }
+  };
+
+  const renderLeftIcon = () => {
+    if (leftIcon === 'home') {
+      return (
+        <View style={{ flex: 1.5 }}>
           <TouchableDebounceDelay
             style={styles.viewLeftHome}
-            onPress={async () => {
-              await SoundService.loadSoundSel('sel.mp3');
-              onClickBackButton();
-            }}>
+            onPress={handleBackPress}
+          >
             <FastImage
               style={{
                 width: moderateScale(45),
@@ -61,47 +79,51 @@ export default class Toolbar extends React.Component {
               source={require('../../resources/images/home.png')}
             />
           </TouchableDebounceDelay>
-        ) : (
+        </View>
+      );
+    } else if (leftIcon === 'back') {
+      return (
+        <View style={{ flex: 1.5 }}>
           <TouchableDebounce
             style={styles.viewLeftBack}
-            onPress={async () => {
-              await SoundService.loadSoundSel('sel.mp3');
-              onClickBackButton();
-            }}>
+            onPress={handleBackPress}
+          >
             <FastImage
-              style={{width: moderateScale(40), height: moderateScale(26)}}
+              style={{ width: moderateScale(40), height: moderateScale(26) }}
               resizeMode={FastImage.resizeMode.cover}
               source={require('../../resources/images/1_12_1.png')}
             />
           </TouchableDebounce>
-        )}
-      </View>
-    );
-  }
+        </View>
+      );
+    } else {
+      return <View style={{ flex: 1.5 }} />;
+    }
+  };
 
-  // param onClickRightButton: callback of right button
-  renderRightIcon(nameRightButton, onClickRightButton) {
+  const renderRightIcon = () => {
     if (nameRightButton === 'create') {
       return (
-        <View style={{flex: 1.5}}>
+        <View style={{ flex: 1.5 }}>
           <TouchableDebounce
             style={styles.viewRight}
-            onPress={() => onClickRightButton()}>
+            onPress={onClickRightButton}
+          >
             <FastImage
               source={require('../../resources/images/1_12_12_0602.png')}
               resizeMode={FastImage.resizeMode.stretch}
               style={styles.btnCreated}
-              // borderRadius={moderateScale(10)}
             />
           </TouchableDebounce>
         </View>
       );
     } else if (nameRightButton === 'home') {
       return (
-        <View style={{flex: 1.5}}>
+        <View style={{ flex: 1.5 }}>
           <TouchableDebounce
             style={styles.viewRight}
-            onPress={() => onClickRightButton()}>
+            onPress={onClickRightButton}
+          >
             <FastImage
               source={require('../../resources/images/home.png')}
               resizeMode={FastImage.resizeMode.stretch}
@@ -110,44 +132,31 @@ export default class Toolbar extends React.Component {
                 height: moderateScale(45),
                 marginEnd: moderateScale(10),
               }}
-              // borderRadius={moderateScale(10)}
             />
           </TouchableDebounce>
         </View>
       );
-    } else if (nameRightButton === 'none') {
-      return <View style={{flex: 1.5}} />;
     } else {
-      return <View />;
+      return <View style={{ flex: 1.5 }} />;
     }
-  }
+  };
 
-  render() {
-    const {
-      title,
-      leftIcon,
-      nameRightButton,
-      onClickBackButton,
-      onClickRightButton,
-    } = this.props;
-    return (
-      <SafeAreaView
-        style={{backgroundColor: Constants.BACKGROUND_COLOR_TOOLBAR}}>
-        <View style={styles.toolbar}>
-          {leftIcon ? this.renderLeftIcon(leftIcon, onClickBackButton) : null}
-          <View style={Styles.toolbar}>
-            <TextFont
-              color="white"
-              content={title}
-              style={Styles.toolbar_title}
-            />
-          </View>
-          {this.renderRightIcon(nameRightButton, onClickRightButton)}
+  return (
+    <SafeAreaView style={{ backgroundColor: Constants.BACKGROUND_COLOR_TOOLBAR }}>
+      <View style={styles.toolbar}>
+        {renderLeftIcon()}
+        <View style={Styles.toolbar}>
+          <TextFont
+            color="white"
+            content={title}
+            style={Styles.toolbar_title}
+          />
         </View>
-      </SafeAreaView>
-    );
-  }
-}
+        {renderRightIcon()}
+      </View>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
   viewLeftHome: {
@@ -176,11 +185,6 @@ const styles = StyleSheet.create({
     backgroundColor: Constants.BACKGROUND_COLOR_TOOLBAR,
     marginEnd: scales.horizontal(10),
   },
-  textRight: {
-    color: 'white',
-    fontSize: moderateScale(20),
-    paddingEnd: moderateScale(5),
-  },
   btnCreated: {
     height: Platform.OS === 'ios' ? verticalScale(35) : verticalScale(40),
     width: Platform.OS === 'ios' ? moderateScale(70) : moderateScale(70),
@@ -190,3 +194,5 @@ const styles = StyleSheet.create({
     borderColor: Color.cl_border_transparent,
   },
 });
+
+export default Toolbar;
